493ddb2516e7caa9d4e6ce8639ef2a7f591b10c9
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2022-05-15 18:23:13 -0400

    add comment

--- app1/src/index.js
+++ app1/src/index.js
@@ -3,4 +3,5 @@ import React from 'react'
 // You can write your own logic here to determine the actual url
 window.app2Url = "http://localhost:3002"
 
+// removing this removes eager error (Uncaught Error: Shared module is not available for eager consumption: webpack/sharing/consume/default/react/react)
 import("app2/component")
