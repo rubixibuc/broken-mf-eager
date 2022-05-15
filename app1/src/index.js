94b24839eda6a7d4dc0e7c589f56df379cc5ffe0
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2022-05-15 18:21:59 -0400

    cleanup

--- app1/src/index.js
+++ app1/src/index.js
@@ -3,8 +3,4 @@ import React from 'react'
 // You can write your own logic here to determine the actual url
 window.app2Url = "http://localhost:3002"
 
-setTimeout(() => {
-    import("app2/component")
-}, 3000)
-
-
+import("app2/component")
